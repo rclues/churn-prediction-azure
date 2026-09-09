6f5e1f17c8873733444397bd5132cf12ffa5750b
rclues <srirammunukutla@gmail.com>
2026-09-09 22:02:34 +0530

    Add scoring script for deployment

---
+++ score.py
@@ -0,0 +1,17 @@
+import json
+import joblib
+import numpy as np
+import os
+
+def init():
+    global model, scaler
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'model.pkl')
+    scaler_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'scaler.pkl')
+    model = joblib.load(model_path)
+    scaler = joblib.load(scaler_path)
+
+def run(raw_data):
+    data = json.loads(raw_data)['data']
+    data_scaled = scaler.transform(np.array(data))
+    predictions = model.predict(data_scaled)
+    return predictions.tolist()
